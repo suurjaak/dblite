12081a8f544c0859d24c617071e3de3c7f77a67d
Erki Suurjaak <erki@lap.ee>
2022-11-18 16:43:01 +0200

    Add __init__.py

---
+++ src/dblite/__init__.py
@@ -0,0 +1,13 @@
+"""
+------------------------------------------------------------------------------
+This file is part of dblite - simple query interface to SQL databases.
+Released under the MIT License.
+
+@author      Erki Suurjaak
+@created     18.11.2022
+@modified    18.11.2022
+"""
+__version__    = "1.0.0"
+__version_date = "2022-11-18"
+
+from . api import *
